7f1345af81d13447a91147ee3a077a53daf7968e
Kevin Chow <chowmein113@gmail.com>
2024-08-11 17:52:06 -0400

    add roar_config for merge
    Signed-off-by: Kevin Chow <chowmein113@gmail.com>

--- roar_config.py
+++ roar_config.py
@@ -1,9 +1,9 @@
 import os
 PORT = 5000
 HOST = "localhost"
-DOWNLOADS_PATH = ...
+DOWNLOADS_PATH = "/home/roar-go/cvat_docker/downloads"
 CVAT_PATH = os.path.join(DOWNLOADS_PATH, 'updates.txt')
-UPLOAD_FOLDER = ...
+UPLOAD_FOLDER = DOWNLOADS_PATH
 PORT = 5000
 FLASK_APP="roar_server.py"
 FLASK_ENV="dev"
