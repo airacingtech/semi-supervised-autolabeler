28dcbbc31638cb9b24b8e6c59a95e787a4a31a30
Kevin Chow <chowmein113@gmail.com>
2024-08-24 13:04:09 -0400

    look into multipool/threading with celery worker
    Signed-off-by: Kevin Chow <chowmein113@gmail.com>

--- start_celery_worker.sh
+++ start_celery_worker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 WORKER_num="$2"
 celery -A roar_server.celery worker --loglevel=info -P eventlet -E -n worker${WORKER_num}
+# celery -A roar_server.celery worker --pool=threads --concurrency=10 --loglevel=info -P eventlet -E -n worker${WORKER_num}
